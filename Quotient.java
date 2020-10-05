11136f0a98b252f799a0ad236ceb8b89b59751c3
cheynoel <61839035+cheynoel@users.noreply.github.com>
2020-10-05 18:45:14 -0400

    Find Quotient and remainder of 2 numbers divided

---
+++ Quotient.java
@@ -0,0 +1,9 @@
+public class Quotient{
+	public static void main(String[] args){
+		int num = 70, num2 = 3;
+		int quotient = num / num2;
+		int remainder = num % num2;
+		System.out.println("Quotient: " + quotient);
+		System.out.println("Remainder: " + remainder);
+	}
+}
